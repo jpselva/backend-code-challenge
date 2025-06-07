b3dff3d4be40d59850b5566035cd6f2b8e8af4f3
JPS <jpsb@duck.com>
2025-06-07 19:18:58 +0200

    Add deserialization of API response

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,53 @@ use axum::{
     routing::get,
 };
 use std::time::Duration;
+use serde::Deserialize;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     let app = Router::new()
         .route("/nodes", get(get_nodes));
 
-    let handler = tokio::spawn(async { pool_nodes().await });
+    tokio::spawn(async { pool_nodes().await });
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn pool_nodes() -> Result<(), reqwest::Error> {
+#[derive(Debug, Deserialize)]
+struct LightningNode {
+    #[serde(rename = "publicKey")]
+    public_key: String,
+    alias: String,
+    channels: u32,
+    capacity: u64,
+}
+
+async fn pool_nodes() {
     loop {
-        tokio::time::sleep(Duration::from_millis(100)).await;
-        let res = reqwest::get("https://mempool.space/api/v1/lightning/nodes/rankings/connectivity")
-            .await?
-            .text()
-            .await?;
+        tokio::time::sleep(Duration::from_millis(1000)).await;
+        let res = request_nodes().await;
+        if let Err(msg) = res {
+            tracing::warn!("Fetching list of nodes failed: {msg}");
+        }
     };
+}
+
+async fn request_nodes() -> Result<Vec<LightningNode>, reqwest::Error> {
+    let res: Vec<LightningNode> = reqwest::get("https://mempool.space/api/v1/lightning/nodes/rankings/connectivity")
+        .await?
+        .json()
+        .await?;
+    Ok(res)
 }
 
 async fn get_nodes() -> &'static str {

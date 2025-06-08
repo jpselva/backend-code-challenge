b9a1bd0ad6d45c6f517bbbb09b59893ac7224d86
JPS <jpsb@duck.com>
2025-06-08 11:00:42 +0200

    Setup database connection

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use axum::{
     Router, 
     routing::get,
+    extract::State,
 };
 use std::time::Duration;
 use serde::Deserialize;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use sqlx::postgres::{PgPool, PgPoolOptions};
 
 #[tokio::main]
 async fn main() {
@@ -16,15 +18,33 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    let db_connection_str = dotenvy::var("DATABASE_URL")
+        .expect("DATABASE_URL not defined");
+
+    let db_pool = PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(&db_connection_str)
+        .await
+        .expect("Can't connect to database");
+
     let nodes_endpoint = dotenvy::var("NODES_ENDPOINT")
         .expect("NODES_ENDPOINT not defined");
-    tokio::spawn(async { pool_nodes(nodes_endpoint).await });
+
+    let db_clone = db_pool.clone();
+    tokio::spawn(async { pool_nodes(nodes_endpoint, db_clone).await });
 
     let app = Router::new()
-        .route("/nodes", get(get_nodes));
+        .route("/nodes", get(get_nodes))
+        .with_state(db_pool);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn get_nodes(State(db_pool): State<PgPool>) -> &'static str {
+    "UNDER CONSTRUCTION"
 }
 
 #[derive(Debug, Deserialize)]
@@ -36,7 +56,7 @@ struct LightningNode {
     capacity: u64,
 }
 
-async fn pool_nodes(endpoint: String) {
+async fn pool_nodes(endpoint: String, db_pool: PgPool) {
     loop {
         tokio::time::sleep(Duration::from_millis(1000)).await;
         let res = request_nodes(&endpoint).await;
@@ -53,9 +73,4 @@ async fn request_nodes(endpoint: &str) -> Result<Vec<LightningNode>, reqwest::Er
         .await?;
     Ok(res)
 }
-
-async fn get_nodes() -> &'static str {
-    "UNDER CONSTRUCTION"
-}
-
 
